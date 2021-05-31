6d3a05b267f2592b184aa00708503884a12abc8e
SELIG-C <carl_selig@hotmail.com>
2021-05-31 20:15:30 +0100

    Folder Re-org
    Signed-off-by: SELIG-C <carl_selig@hotmail.com>

--- level2.py
+++
@@ -1,9 +0,0 @@
-def parser(string):
-    alphabets = 'abcdefghijklmnopqrstuvwxyz'
-    shiftedAlphabets = alphabets[2:len(string)] + alphabets[0:2]
-    print(shiftedAlphabets)
-    table = string.maketrans(alphabets, shiftedAlphabets)
-    return string.translate(table)
-
-
-print(parser("http://www.pythonchallenge.com/pc/def/map.html"))
